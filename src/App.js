b7916991a913b2114485085305a48d0857156549
Sushant <sushantshekhar01011999@gmail.com>
2024-12-26 17:48:15 +0530

    Navbar testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import { Button } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './component/Home';
+import About from './component/About';
+import Projects from './component/Projects';
+import Resume from './component/Resume';
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="outlined">Outlined</Button>
-      <div>This is for cheking</div>
-      <div>another test</div>
+    <div >
+    <Router>
+      <Routes>
+        <Route path='/'  element={<Home/>}/>
+        <Route path='/about'  element={<About/>}/>
+        <Route path='/projects'  element={<Projects/>}/>
+        <Route path='/resume'  element={<Resume/>}/>
+      </Routes>
+    </Router>
     </div>
   );
 }
---
+++ src/component/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './navbar.css'
+import { Box } from '@mui/material';
+import Link from '@mui/material/Link';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+
+
+export default function Navbar() {
+    return (
+        <Box sx={{ backgroundColor: '#6f42c1', display: 'flex', justifyContent: 'flex-end', width: '100%', height: 90, alignItems: 'center' }}>
+            <HomeIcon sx={{ marginRight: 1, color: 'white' }} />
+            <Link href="/" sx={{ color: 'white', marginRight: 5, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 800 }}>Home</Link>
+            <AccountCircleIcon sx={{ marginRight: 1, color: 'white' }} />
+            <Link href="/about" sx={{ color: 'white', marginRight: 5, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 800 }}>About</Link>
+            <LiveTvIcon sx={{ marginRight: 1, color: 'white' }} />
+            <Link href="/projects" sx={{ color: 'white', marginRight: 5, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 800 }}>Projects</Link>
+            <ContentPasteIcon sx={{ marginRight: 1, color: 'white' }} />
+            <Link href="/resume" sx={{ color: 'white', marginRight: 5, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 800 }}>Resume</Link>
+        </Box>
+    )
+}
