caffbb56830aaa09a898f7e9c5d3e9ee683addbf
Sushant <sushantshekhar01011999@gmail.com>
2024-12-28 17:32:36 +0530

    some changes

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import HomeLogo from '../image/home-bg.jpg';
 import StudyImage from '../image/study.jpg';
-import Navbar from './Navbar';
 
 export default function Home() {
     return (
