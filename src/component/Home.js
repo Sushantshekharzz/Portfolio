1993f1b70ce25eca12ed27c6fc165c4ee10416f2
Sushant <sushantshekhar01011999@gmail.com>
2024-12-30 08:06:36 +0530

    Added Icon SS To navbar

--- src/component/Home.js
+++ src/component/Home.js
@@ -49,7 +49,7 @@ export default function Home() {
                             </Box>
                         </Box>
                         <Typography
-                            sx={{ fontFamily: 'Raleway,serif', fontSize: 40, color: 'white', paddingTop: { xs: 3, md: 1 } , textAlign:'center' }}
+                            sx={{ fontFamily: 'Raleway,serif', fontSize: 40, color: 'white', paddingTop: { xs: 3, md: 1 }, textAlign: 'center' }}
                         >I'M <Typography component="span" sx={{ fontFamily: 'Raleway,serif', color: "#c770f0", fontWeight: 1000, fontSize: 40, }}>Sushant Shekhar</Typography></Typography>
                         <Box sx={{
                             fontFamily: 'Raleway,serif', color: 'white', paddingTop: 9
@@ -68,19 +68,19 @@ export default function Home() {
                     </Box>
                 </Box>
                 <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-                    <Typography sx={{ fontFamily: 'Raleway,serif', fontSize: 40, color: 'white', paddingTop: { xs: 3, md: 6 } , textAlign:'center'}} >LET ME <Typography component="span" sx={{ fontFamily: 'Raleway,serif', color: '#c770f0', fontWeight: 1000, fontSize: 40, }}>INTRODUCE</Typography> MYSELF</Typography>
+                    <Typography sx={{ fontFamily: 'Raleway,serif', fontSize: 40, color: 'white', paddingTop: { xs: 3, md: 6 }, textAlign: 'center' }} >LET ME <Typography component="span" sx={{ fontFamily: 'Raleway,serif', color: '#c770f0', fontWeight: 1000, fontSize: 40, }}>INTRODUCE</Typography> MYSELF</Typography>
                     <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: { md: 'row', xs: 'column' }, alignItems: 'center', paddingTop: { md: 1, xs: 5 } }}>
                         <Box >
-                            <Typography sx={{ fontFamily: 'Roboto Condensed', color: 'white', fontSize: 25 , marginTop:4}}>I fell in love with <Typography  variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0",  fontSize: 25,fontStyle: 'italic' }}> Programming </Typography>and I have at least learnt something, I think… 🤷‍♂️</Typography>
+                            <Typography sx={{ fontFamily: 'Roboto Condensed', color: 'white', fontSize: 25, marginTop: 4 }}>I fell in love with <Typography variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0", fontSize: 25, fontStyle: 'italic' }}> Programming </Typography>and I have at least learnt something, I think… 🤷‍♂️</Typography>
 
-                            <Typography sx={{ fontFamily: 'Roboto Condensed', color: 'white', fontSize: 25 ,marginTop:4}}>Skilled in developing dynamic, and robust web applications with <Typography variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0",  fontSize: 25,fontStyle: 'italic' }}>React</Typography> for frontend and  <Typography variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0",  fontSize: 25, fontStyle: 'italic'}}>Node.js</Typography> for backend development.</Typography>
+                            <Typography sx={{ fontFamily: 'Roboto Condensed', color: 'white', fontSize: 25, marginTop: 4 }}>Skilled in developing dynamic, and robust web applications with <Typography variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0", fontSize: 25, fontStyle: 'italic' }}>React</Typography> for frontend and  <Typography variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0", fontSize: 25, fontStyle: 'italic' }}>Node.js</Typography> for backend development.</Typography>
 
-                            <Typography sx={{ fontFamily: 'Roboto Condensed', color: 'white', fontSize: 25, marginTop:4}}>Expert in integrating and managing both<Typography variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0",  fontSize: 25,fontStyle: 'italic' }}> SQL and NoSQL Databases </Typography >for optimized data storage and retrieval.</Typography>
+                            <Typography sx={{ fontFamily: 'Roboto Condensed', color: 'white', fontSize: 25, marginTop: 4 }}>Expert in integrating and managing both<Typography variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0", fontSize: 25, fontStyle: 'italic' }}> SQL and NoSQL Databases </Typography >for optimized data storage and retrieval.</Typography>
 
-                            <Typography sx={{ fontFamily: 'Roboto Condensed', color: 'white', fontSize: 25,marginTop:4 }}>Passionate about building<Typography variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0",  fontSize: 25, fontStyle: 'italic' }}> User-Centric and Scalable Solutions</Typography>  with a focus on<Typography variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0",  fontSize: 25, fontStyle: 'italic'}}> Performance and Maintainability.</Typography> </Typography>
+                            <Typography sx={{ fontFamily: 'Roboto Condensed', color: 'white', fontSize: 25, marginTop: 4 }}>Passionate about building<Typography variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0", fontSize: 25, fontStyle: 'italic' }}> User-Centric and Scalable Solutions</Typography>  with a focus on<Typography variant='span' sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0", fontSize: 25, fontStyle: 'italic' }}> Performance and Maintainability.</Typography> </Typography>
                         </Box>
-                        <Box sx={{ paddingTop: { xs: 4 } }}>
-                            <img   alt="" src={Introduction} style={{ height: '100%', maxWidth: '100%', objectFit: 'cover' }} />
+                        <Box sx={{ paddingTop: { xs: 8 } }}>
+                            <img alt="" src={Introduction} style={{ height: '100%', maxWidth: '100%', objectFit: 'cover' }} />
                         </Box>
                     </Box>
                 </Box>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Box } from '@mui/material';
 import Link from '@mui/material/Link';
 import HomeIcon from '@mui/icons-material/Home';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
@@ -12,6 +11,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import { Box, Typography } from '@mui/material';
 
 
 
@@ -25,29 +25,42 @@ export default function Navbar() {
     return (
         <>
             <Box
-                sx={{ display: { md: 'none' , xs:'flex'}, 
-                backgroundColor: 'rgba(138, 38, 210, 0.73)', // Same background color as web navbar
-                width: '100%', // Full width
-                paddingInline: 1, // Add horizontal padding
-                justifyContent:'flex-end',
-                boxSizing: 'border-box', // Ensure padding doesn't add to the width
+                sx={{
+                    display: { md: 'none', xs: 'flex' },
+                    backgroundColor: 'rgba(138, 38, 210, 0.73)', // Same background color as web navbar
+                    width: '100%', // Full width
+                    paddingInline: 1, // Add horizontal padding
+                    justifyContent: 'flex-end',
+                    boxSizing: 'border-box', // Ensure padding doesn't add to the width
 
-                position: 'fixed', top: 0,
-                 height: 52 }}
+                    position: 'fixed', top: 0,
+                    height: 52
+                }}
                 onClick={toggleDrawer}
             >
                 <MenuIcon sx={{ fontSize: 50, marginLeft: 1, color: 'white' }} />
             </Box>
-            <Box sx={{ position: 'fixed', top: 0, backgroundColor: 'rgba(138, 38, 210, 0.07)', display: { xs: 'none', sm: 'none', md: 'flex' }, justifyContent: 'flex-end', width: '100%', height: 90, alignItems: 'center' }}>
-                <HomeIcon sx={{ marginRight: 1, color: 'white' }} />
-                <Link href="/" sx={{ color: 'white', marginRight: 10, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 500, fontSize: 20 }}>Home</Link>
-                <AccountCircleIcon sx={{ marginRight: 1, color: 'white' }} />
-                <Link href="/about" sx={{ color: 'white', marginRight: 10, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 500, fontSize: 20 }}>About</Link>
-                <LiveTvIcon sx={{ marginRight: 1, color: 'white' }} />
-                <Link href="/projects" sx={{ color: 'white', marginRight: 10, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 500, fontSize: 20 }}>Projects</Link>
-                <ContentPasteIcon sx={{ marginRight: 1, color: 'white' }} />
-                <Link href="/resume" sx={{ color: 'white', marginRight: 10, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 500, fontSize: 20 }}>Resume</Link>
+            <Box sx={{ position: 'fixed', top: 0, backgroundColor: 'rgba(138, 38, 210, 0.07)', display: { xs: 'none', sm: 'none', md: 'flex' }, justifyContent: 'flex-start', width: '100%', height: 90, alignItems: 'center' }}>
+                <Box
+                    sx={{ width: '100%', paddingLeft: 4 }}>
+                    <Typography
+                        sx={{
+                            fontFamily: 'Raleway,serif', color: "#c770f0", fontWeight: 1000, fontSize: 30,
+                        }}
+                    >SS.</Typography>
+                </Box>
+                <Box sx={{ display: { md: 'flex' }, justifyContent: 'flex-end', width: '100%', alignItems: 'center' }}>
+                    <HomeIcon sx={{ marginRight: 1, color: 'white' }} />
+                    <Link href="/" sx={{ color: 'white', marginRight: 10, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 500, fontSize: 20 }}>Home</Link>
+                    <AccountCircleIcon sx={{ marginRight: 1, color: 'white' }} />
+                    <Link href="/about" sx={{ color: 'white', marginRight: 10, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 500, fontSize: 20 }}>About</Link>
+                    <LiveTvIcon sx={{ marginRight: 1, color: 'white' }} />
+                    <Link href="/projects" sx={{ color: 'white', marginRight: 10, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 500, fontSize: 20 }}>Projects</Link>
+                    <ContentPasteIcon sx={{ marginRight: 1, color: 'white' }} />
+                    <Link href="/resume" sx={{ color: 'white', marginRight: 10, textDecoration: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 500, fontSize: 20 }}>Resume</Link>
+                </Box>
             </Box>
+
             <Drawer open={toggle} onClose={toggleDrawer} anchor='top'>
                 <List sx={{ backgroundColor: '#6f42c1', }}>
                     <ListItem >
