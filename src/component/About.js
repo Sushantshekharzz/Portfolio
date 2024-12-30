333a671ed0905ec86f61728961106a718763fd10
Sushant <sushantshekhar01011999@gmail.com>
2024-12-30 22:12:52 +0530

    alignment changes

--- src/component/About.js
+++ src/component/About.js
@@ -48,7 +48,7 @@ export default function About() {
                 sx={{ fontFamily: 'Roboto Condensed', fontSize: 25, color: 'white', }}
               >I completed my <Typography component="span" sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0", fontWeight: 200, fontSize: 25, }}>B.Tech</Typography> from <Typography component="span" sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0", fontWeight: 200, fontSize: 25, }}>Asansol Engineering College.</Typography></Typography>
             </Box>
-            <Box sx={{ paddingTop: { md: 20, xs: 10 } }}>
+            <Box sx={{ paddingTop: { md: 10, xs: 10 } }}>
               <Typography
                 sx={{ fontFamily: 'Roboto Condensed', fontSize: 25, color: 'white', }}
               >Apart from coding, some other activities that I love to do!</Typography>
@@ -65,28 +65,34 @@ export default function About() {
                     sx={{ fontFamily: 'Roboto Condensed', fontSize: 25, color: 'white', paddingLeft: 2 }}
                   >Watching Movie</Typography>
                 </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column', marginTop: { xs: 6, md: 5 } ,width:'100%' }}>
+        <Box sx={{textAlign:{xs:'center', md: 'left'}}} >
+          <Typography component="span" sx={{ fontFamily: 'Roboto Condensed', color: "white", fontSize: 25, fontStyle: 'italic' }}>"Compassion is the greatest virtue."</Typography>
+          <Box sx={{ display: 'flex', justifyContent: {md:'flex-start', xs:'center'}, alignItems: {md:'center', xs:'flex-start'} }}>
+            <HorizontalRuleSharpIcon sx={{ color: "#c770f0", fontSize: 15, marginTop: 3 }} />
+            <Typography component="span" sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0", fontSize: 15, marginTop: 3, }}>Sushant Shekhar</Typography>
+          </Box>
+        </Box>
+      </Box>
               </Box>
             </Box>
           </Box>
         </Box>
         <Box sx={{
           height: '100%', width: '100%',
-          paddingBottom: { md: 40, xs: 0 },
+          paddingBottom: { md: 25, xs: 0 },
           paddingTop: { xs: 4, },
           maxWidth: { xs: '100%', md: '400px' }, // Full width on mobile, limited on desktop
         }}>
           <img src={about} alt="" style={{ height: '100%', maxWidth: '100%', objectFit: 'cover' }} />
         </Box>
       </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginTop: { xs: 10, md: 0 } }}>
-        <Box sx={{ textAlign: 'center' }}>
-          <Typography component="span" sx={{ fontFamily: 'Roboto Condensed', color: "white", fontSize: 25, fontStyle: 'italic' }}>"Compassion is the greatest virtue."</Typography>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-            <HorizontalRuleSharpIcon sx={{ color: "#c770f0", fontSize: 15, marginTop: 3 }} />
-            <Typography component="span" sx={{ fontFamily: 'Roboto Condensed', color: "#c770f0", fontSize: 15, marginTop: 3, }}>Sushant Shekhar</Typography>
-          </Box>
-        </Box>
+    
+      <Box sx={{textAlign:'center',paddingTop:5}}> 
+                            <Typography sx={{ fontFamily: 'Raleway,serif', fontSize: 40, color: 'white' }} >Professional <Typography component="span" sx={{ fontFamily: 'Raleway,serif', color: '#c770f0', fontWeight: 1000, fontSize: 40, }}>Skillset</Typography></Typography>
+        
       </Box>
+     
     </Box>
   )
 }
